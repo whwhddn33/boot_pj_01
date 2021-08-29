9b8c8f6c4a88f0ba8e7246221baff2aa098ca06b
whwhddn33 <whwhddn33@naver.com>
2021-08-30 00:59:11 +0900

    hello boot

---
+++ src/main/java/info/jongwoo/Boot_pj_01/BootPj01Application.java
@@ -0,0 +1,13 @@
+package info.jongwoo.Boot_pj_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPj01Application.class, args);
+	}
+
+}
---
+++ src/main/java/info/jongwoo/Boot_pj_01/ServletInitializer.java
@@ -0,0 +1,13 @@
+package info.jongwoo.Boot_pj_01;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootPj01Application.class);
+	}
+
+}
