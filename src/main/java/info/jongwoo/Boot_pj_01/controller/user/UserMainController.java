90b2c10c3618570a354b468a80a874ee26976b75
joy <duio0oiub@naver.com>
2021-09-01 22:06:36 +0900

    메인에 joy merge함

--- src/main/java/info/jongwoo/Boot_pj_01/controller/user/UserMainController.java
+++ src/main/java/info/jongwoo/Boot_pj_01/controller/user/UserMainController.java
@@ -5,7 +5,6 @@ import info.jongwoo.Boot_pj_01.domain.UserDTO;
 import info.jongwoo.Boot_pj_01.service.UserSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +17,7 @@ public class UserMainController {
     private UserSerivce userService;
 
     @RequestMapping({"/"})
-    public ModelAndView main(Model model) throws Exception {
+    public ModelAndView main() throws Exception {
         ModelAndView modelAndView = new ModelAndView("/main"+Const.uTiles);
 
         UserDTO userDTO = new UserDTO();
